fe5d191a8a3fcec6fe74fc707190ca3d492f23d1
Vlad Viderko <summervibeswith9pm@gmail.com>
2023-01-05 17:29:34 +0100

    Create road

--- main.js
+++ main.js
@@ -1,18 +1,26 @@
 const canvas = document.querySelector('#root');
+
+canvas.width = 200;
+
 const context = canvas.getContext('2d');
-const car = new Car(100, 100, 30, 50);
+
+const road = new Road(canvas.width / 2, canvas.width * 0.9);
+const car = new Car(road.getLaneCenter(1), 100, 30, 50);
 
 const animate = () => {
-  canvas.height = window.innerHeight;
-  canvas.width = 200;
-
   car.update();
+
+  canvas.height = window.innerHeight;
+
+  context.save();
+  context.translate(0, -car.y + canvas.height * 0.8);
+
+  road.draw(context);
   car.draw(context);
+
+  context.restore();
 
   requestAnimationFrame(animate);
 }
-
-
-car.draw(context);
 
 animate();
